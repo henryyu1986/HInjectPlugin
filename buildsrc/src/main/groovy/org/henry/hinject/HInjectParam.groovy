7d3dd68977c7485b6207b732d56c390b635e84a5
Henry Yu <henryyu1986@163.com>
2017-04-20 16:03:32 +0800

    ignore some file

--- buildsrc/src/main/groovy/org/henry/hinject/HInjectParam.groovy
+++
@@ -1,9 +0,0 @@
-package org.henry.hinject
-
-public class HInjectParam {
-    boolean debug = true
-    IdentityHashMap<String, Object> injectMethod = [:]
-    IdentityHashMap<String, Object> injectVariables = [:]
-    IdentityHashMap<String, Object> inject2Method = [:]
-    String packageName
-}
